a247d23c455b1f06b16994035130911d271a6f1f
Quezoo <116873145+Quezoo@users.noreply.github.com>
2022-10-28 19:06:44 +0200

    Add files via upload

---
+++ Zmienne.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Witaj Filip" << endl;
+	cout << endl << "Podaj numer PIN" << endl;
+	int pin;
+	cin >> pin;
+
+	if (pin == 6919)
+	{
+		cout << "Poprawny pin!" << endl;
+	}
+	else
+	{
+		cout << "Niepoprawny pin!" << endl;
+	}
+	return 0;
+}
